31fc1526e624d81049f09b88e6101d60ca3533ac
KARTHIKEYA S K <161928865+NOSEBLE3D@users.noreply.github.com>
2025-04-15 20:20:14 +0530

    Add files via upload

---
+++ FIFO/Edge Detection/bothedge_det.v
@@ -0,0 +1,34 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 24.02.2025 20:42:14
+// Design Name: 
+// Module Name: bothedge_det
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module bothedge_det(pos_det,din,clk);
+
+input clk,din;
+output pos_det;
+reg q;
+
+always@(posedge clk)begin
+    q <= din;
+end
+
+assign pos_det = din ^ q;
+endmodule
